4f3742f67b78afb47ce9e67a065b3292f7acaecc
Paulo Robson <p.robson085@gmail.com>
2023-09-18 22:36:41 -0300

    feat: adicionando interface

--- src/app/components/pensamentos/criar-pensamento/criar-pensamento.component.ts
+++ src/app/components/pensamentos/criar-pensamento/criar-pensamento.component.ts
@@ -1,13 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Pensamento } from '../pensamento';
 
 @Component({
   selector: 'app-criar-pensamento',
   templateUrl: './criar-pensamento.component.html',
   styleUrls: ['./criar-pensamento.component.css'],
 })
+
 export class CriarPensamentoComponent implements OnInit {
-  pensamento = {
+  pensamento: Pensamento = {
     id: 1,
     conteudo: 'Aprendendo Angular',
     autoria: 'Dev',
--- src/app/components/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ src/app/components/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -1,29 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { Pensamento } from '../pensamento';
 
 @Component({
   selector: 'app-listar-pensamento',
   templateUrl: './listar-pensamento.component.html',
   styleUrls: ['./listar-pensamento.component.css'],
 })
+
 export class ListarPensamentoComponent implements OnInit {
-  listaPensamentos = [
-    {
-      conteudo: 'Passo informacoes para o componente filho',
-      autoria: 'Angular - Componente pai',
-      modelo: 'modelo2',
-    },
-    {
-      conteudo: 'Minha propriedade decorada com o @Input',
-      autoria: 'Angular - componente filho',
-      modelo: 'modelo3',
-    },
-    {
-      conteudo:
-        'Mussum Ipsum, cacilds vidis litro abertis. Posuere libero varius. Nullam a nisl ut ante blandit hendrerit. Aenean sit amet nisi. Diuretics paradis num copo é motivis de denguis. Atirei o pau no gatis, per gatis num morreus. Interessantiss quisso pudia ce receita de bolis, mais bolis eu num gostis. Sapien in monti palavris qui num significa nadis i pareci latim. ',
-      autoria: '',
-      modelo: 'modelo1',
-    },
-  ];
+  listaPensamentos: Pensamento[] = [];
 
   constructor() {}
 
