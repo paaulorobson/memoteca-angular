c746fa15187c53c19bd54f315dc747df2bdc6ceb
Paulo Robson <p.robson085@gmail.com>
2023-09-21 23:17:26 -0300

    feat: adicionando filtro

--- src/app/components/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ src/app/components/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -12,18 +12,19 @@ export class ListarPensamentoComponent implements OnInit {
   listaPensamentos: Pensamento[] = [];
   paginaAtual:number = 1;
   haMaisPensamentos: boolean = true;
+  filtro: string = '';
 
   constructor(private service: PensamentoService) {}
 
   ngOnInit(): void {
-    this.service.listar(this.paginaAtual).subscribe((listaPensamentos) => {
+    this.service.listar(this.paginaAtual, this.filtro).subscribe((listaPensamentos) => {
       this.listaPensamentos = listaPensamentos
     });
   }
 
   carregarMaisPensamentos() {
-    this.service.listar(++this.paginaAtual).subscribe(() => {
-      this.listaPensamentos.push(...this.listaPensamentos);
+    this.service.listar(++this.paginaAtual, this.filtro).subscribe((listaPensamentos) => {
+      this.listaPensamentos.push(...listaPensamentos);
       if(!this.listaPensamentos.length) {
         this.haMaisPensamentos = false;
       }
@@ -32,5 +33,13 @@ export class ListarPensamentoComponent implements OnInit {
 
   possuiPensamento(): boolean {
     return this.listaPensamentos.length > 0;
+  }
+
+  pesquisarPensamentos() {
+    this.haMaisPensamentos = true;
+    this.paginaAtual = 1
+    this.service.listar(this.paginaAtual, this.filtro).subscribe((listaPensamentos) => {
+      this.listaPensamentos = listaPensamentos
+    })
   }
 }
